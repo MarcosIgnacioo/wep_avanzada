fdf2eb3b5f57dbe02c0d256b7c2002caf1bee02c
MarcosIgnacioo <magma_ruby145@hotmail.com>
2024-09-10 18:38:41 -0700

    vueowowo

--- vueowowowoow/src/App.vue
+++ vueowowowoow/src/App.vue
@@ -1,30 +1,40 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue';
+const count = ref(0);
+const name = ref("your nameee..");
+const first_num = ref(0);
+const second_num = ref(0);
+const sum = ref(0);
+
+function name_typing(e: any) {
+  name.value = e.target.value
+}
+
+function add() {
+  sum.value = first_num.value + second_num.value;
+}
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="inputs">
+    <label>INgresa tu nombre</label>
+    <input @keyup="name_typing">
+    <h1>{{ name }}</h1>
+    <label>pon dos numeros no mas!!!!</label>
+    <!-- @keyup="add" -->
+    <!-- @keyup="add"     -->
+    <input v-model.number="first_num">
+    <input v-model.number="second_num">
+    <button @click="add">suma</button>
+    <h1>{{ sum }}</h1>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <button @click="count++">{{ count }}</button>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+#inputs {
+  display: flex;
+  flex-direction: column;
 }
 </style>
